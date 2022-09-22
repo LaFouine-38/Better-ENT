ad9f55677060331fe8781e28a5ab66f11f1eb414
Demonloup <demonloup2313@gmail.com>
2022-09-22 20:58:12 +0200

    a

--- scripts/handlers/backgroundScriptInit.js
+++ scripts/handlers/backgroundScriptInit.js
@@ -1,5 +1,5 @@
 try {
-    importScripts("./../background_scripts/updatePageEventHandler.js", "./../background_scripts/urlHandler.js");
+    importScripts("./../background_scripts/updatePageEventHandler.js");
 } catch (e) {
     console.log(e);
 }
