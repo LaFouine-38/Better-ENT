eeca242607f5c7b6e766260b76f689f962b2e01c
LaFouine-38 <86793823+LaFouine-38@users.noreply.github.com>
2022-09-24 03:10:47 +0200

    Update backgroundScriptInit.js

--- scripts/handlers/backgroundScriptInit.js
+++ scripts/handlers/backgroundScriptInit.js
@@ -1,9 +1,5 @@
 try {
-<<<<<<< Updated upstream
-    importScripts("./../background_scripts/updatePageEventHandler.js");
-=======
     importScripts("./../background_scripts/updatePageEventHandler.js", "./../background_scripts/connexionUrlHandler.js");
->>>>>>> Stashed changes
 } catch (e) {
     console.log(e);
 }
