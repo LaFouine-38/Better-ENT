be8e85ebb96285280609add34116f64179bc0219
LaFouine-38 <86793823+LaFouine-38@users.noreply.github.com>
2022-09-24 13:08:34 +0200

    Update connexion4.js

--- scripts/connection/connexion4.js
+++ scripts/connection/connexion4.js
@@ -1,7 +1,6 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.sender == 'connexionUrl' && request.state == 'connexion4'){
         document.querySelector('div.msg__content > p.p-like > strong > a').click()
-
         chrome.runtime.sendMessage({sender: "connexion4", tabId:request.tabId, infos: request.infos})
     }
 })
