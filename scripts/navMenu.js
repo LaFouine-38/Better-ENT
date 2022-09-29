d24d7522d98c5b1e63ee2987d31b10d3e97e2447
LaFouine-38 <86793823+LaFouine-38@users.noreply.github.com>
2022-09-29 19:14:54 +0200

    update background navmenu items
    la div background est décalée de 1 rang en bas jsp pk

--- scripts/navMenu.js
+++ scripts/navMenu.js
@@ -1,6 +1,6 @@
-window.onload = () => {
-    const shortcuts = document.querySelectorAll('nav.menu.js-menu > ul.services-shortcut > li.services-shortcut__item')
-    shortcuts.forEach((e) => {
-        e.appendChild('<div class="shortcut-background"></div>')
-    })
-}
+const shortcuts = document.querySelectorAll('li.services-shortcut__item')
+shortcuts.forEach((e) => {
+    let element = document.createElement('div')
+    element.classList += "shortcut-background"
+    e.appendChild(element)
+})
