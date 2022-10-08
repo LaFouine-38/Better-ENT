5f6a1c0a8cf163763693b016b1641a072669a5c3
LaFouine-38 <86793823+LaFouine-38@users.noreply.github.com>
2022-10-08 18:30:51 +0200

    Update pour les futures majs de l'extension
    ajout et modifications des fichiers concernant l'update

--- scripts/handlers/backgroundScriptInit.js
+++ scripts/handlers/backgroundScriptInit.js
@@ -1,6 +1,6 @@
 try {
     importScripts("./../background_scripts/updatePageEventHandler.js", "./../background_scripts/connexionUrlHandler.js", "./../background_scripts/fileLoader.js");
-    importScripts("./../config/config.js")
+    importScripts("./../config/config.js", "./../background_scripts/updateScriptBg.js")
 } catch (e) {
     console.log(e);
 }
