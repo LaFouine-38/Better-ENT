69850fd09f51c4d53660b2c4020b727b92450703
blueskin8 <garciaalbanrot@gmail.com>
2022-10-10 17:23:50 +0200

    Update fileLoader.js
    il etait impossible d'importer des fichiers js avec fileLoader.js donc il faut désormais les imports dans le manifest

--- scripts/background_scripts/fileLoader.js
+++ scripts/background_scripts/fileLoader.js
@@ -1,42 +1,6 @@
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-    /*if (tab.status == "complete") {
 
-        //                  JS                  //
-
-        if (tab.url.startsWith('https://fleming-isere.ent.auvergnerhonealpes.fr/')) {
-            await chrome.scripting.executeScript({
-                target: { tabId: tabId },
-                files: ["scripts/emploiDuTemps.js", "scripts/handlers/requestHandler.js"]
-            })
-        }
-        if (tab.url == "https://fleming-isere.ent.auvergnerhonealpes.fr/") {
-            await chrome.scripting.executeScript({
-                target: { tabId: tabId },
-                files: ["scripts/connection/connexion1.js"]
-            })
-        }
-        if (tab.url == "https://cas.ent.auvergnerhonealpes.fr/login?service=https%3A%2F%2Ffleming-isere.ent.auvergnerhonealpes.fr%2Fsg.do%3FPROC%3DIDENTIFICATION_FRONT") {
-            await chrome.scripting.executeScript({
-                target: { tabId: tabId },
-                files: ["scripts/connection/connexion2.js"]
-            })
-        }
-        if (tab.url == "https://cas.ent.auvergnerhonealpes.fr/saml/SAMLAssertionConsumer") {
-            await chrome.scripting.executeScript({
-                target: { tabId: tabId },
-                files: ["scripts/connection/connexion4.js"]
-            })
-        }
-
-        //ajouter les autres pages en suivant le modele
-        //mettre tab.url == "lien" au lieu de startsWith("") si il faut un lien exact
-        /* if(tab.url.startsWith('match-pattern-sans-etoile')){
-            await chrome.scripting.executeScript({
-                target: {tabId: tabId},
-                files: ["lienDuFichier(le meme que dans manifest.json)", "c'est vraiment utile que je réexplique ?"]
-            })
-        }
-    }*/
+    //                  IMPORTANT
 
     if (tab.status == "loading") {
 
