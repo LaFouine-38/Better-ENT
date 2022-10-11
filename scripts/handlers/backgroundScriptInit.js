4ef584344495ba645e1727be09f5e76cb21f79db
blueskin8 <garciaalbanrot@gmail.com>
2022-10-11 21:18:14 +0200

    debut de la fonction de la maj auto
    ajout des fichiers et test qui ne foncionnent pas (rip ...)

--- scripts/handlers/backgroundScriptInit.js
+++ scripts/handlers/backgroundScriptInit.js
@@ -1,6 +1,6 @@
 try {
     importScripts("./../background_scripts/updatePageEventHandler.js", "./../background_scripts/connexionUrlHandler.js", "./../background_scripts/fileLoader.js");
-    importScripts("./../config/config.js", "./../background_scripts/updateScriptBg.js")
+    importScripts("./../config/config.js", "./../background_scripts/updateScriptBg.js", "./../background_scripts/MajAuto/checkMaj.js");
 } catch (e) {
     console.log(e);
 }
